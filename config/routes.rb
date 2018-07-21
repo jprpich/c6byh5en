e4684387231036a63fa1e72a2944f453a1f6ac80
Joshua <joshuaprpich@gmail.com>
2018-07-21 12:10:16 -0500

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json }, path: '/'  do
+  namespace :api, defaults: { format: :json }, path: '/api/v1'  do
     scope module: :v1 do
        resources :products
     end
