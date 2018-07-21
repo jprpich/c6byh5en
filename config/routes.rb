6658ad2fe27ab318cb98b312e74cb8aae07b70ce
Joshua <joshuaprpich@gmail.com>
2018-07-21 12:04:37 -0500

    fix error

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :products
+  namespace :api, defaults: { format: :json }, path: '/'  do
+    scope module: :v1 do
+       resources :products
+    end
+  end
 
-get 'api/v1/products', to: 'products#index'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
